29d5724f7e9dc1f2c8691632822221270e09eafc
Austin Young <austineyoung2000@gmail.com>
2025-08-09 00:53:28 -0400

    bluejay: Updates device configuration
    Configures the device for BCR support by enabling prebuilt BCR packages.
    
    Removes unnecessary configurations related to Pixel Launcher,
    old Google Camera and Lawnchair.
    
    Sets a device specific maintainer and version.

--- lineage_bluejay.mk
+++ lineage_bluejay.mk
@@ -26,23 +26,16 @@ $(call inherit-product, vendor/bcr/bcr.mk)
 
 # Addons
 TARGET_HAS_UDFPS := true
+TARGET_PREBUILT_BCR := true
 
 # Gms 
 WITH_GMS := true
-
-# Ship Pixel Launcher
-TARGET_DEFAULT_PIXEL_LAUNCHER := true
-
-# Ship Google Camera
-TARGET_PREBUILT_GOOGLE_CAMERA := true
-
-# Lawnchair Prebuilt
-TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
 
 # disable/enable blur support, default is false
 TARGET_ENABLE_BLUR := true
 
 RISING_MAINTAINER=EliteDarkKaiser
+RISING_VERSION := 8.0
     
 # Use Scudo instead of Jemalloc
 PRODUCT_USE_SCUDO := true
